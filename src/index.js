9bf6767887652fa7015c4e1ddabab9981b5d01df
◦•●◉ԲμҬμЯ૯ ૮ଇԁ૯Я◉●•◦ <81414536+7coder79@users.noreply.github.com>
2021-08-24 20:07:24 +0500

    Start Working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+// import App from './App';
+
+i18n
+  .use(initReactI18next) 
+  .init({
+    resources: {
+      en: {
+        translation: {
+          "Welcome to React": "Welcome to React and react-i18next"
+        }
+      },
+      fr: {
+        translation: {
+          "Welcome to React": "Bienvenue a React at react-i18next"
+        }
+      }
+    },
+    lng: "en", 
+    fallbackLng: "en",
+    detection: {
+      order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
+      cash: ['cookie']
+    }
+  });
+
+function App() {
+  const { t } = useTranslation();
+
+  return <h2>{t('Welcome to React')}</h2>
+}
 
 ReactDOM.render(
   <React.StrictMode>
