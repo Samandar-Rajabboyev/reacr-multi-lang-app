918f7d16be431681e3bf19c91c8ad0a42e2b8518
◦•●◉ԲμҬμЯ૯ ૮ଇԁ૯Я◉●•◦ <81414536+7coder79@users.noreply.github.com>
2021-08-25 08:50:53 +0500

    set dropdown btn changeLanguage function

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import i18n from 'i18next';
-import { useTranslation, initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import HttpApi from 'i18next-http-backend'
-// import App from './App';
+import { useTranslation, initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'flag-icon-css/css/flag-icon.min.css';
 
 i18n
   .use(initReactI18next) 
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
+    supportedLngs: ['en', 'fr', 'ar', 'uz'],
     fallbackLng: "en",
     detection: {
-      order: ['htmlTag', 'cookie', 'localStorage',  'subdomain', 'path'],
-      cashes: ['cookie']
+      order: [ 'cookie', 'htmlTag', 'localStorage',  'subdomain', 'path'],
+      caches: ['cookie']
     },
     backend: {
-      loadPath: 'assets/locales/{{lang}}/translation.json'
+      loadPath: '/assets/locales/{{lng}}/translation.json'
     },
     react: {useSuspense: false}
   });
 
-function App() {
-  const { t } = useTranslation();
-
-  return <h2>{t('welcome_to_react')}</h2> 
-}
 
 ReactDOM.render(
   <React.StrictMode>
